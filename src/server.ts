f034d1acb6863b16ee8d36fa207564fa905a8382
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-07-08 19:39:39 -0300

    chore: add ultracite biome config preset

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,23 @@
 import { fastifyCors } from '@fastify/cors';
 import { fastify } from 'fastify';
 import {
-	serializerCompiler,
-	validatorCompiler,
-	type ZodTypeProvider
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-	origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173'
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => {
-	return 'OK';
+  return 'OK';
 });
 
-app.listen({ port: env.PORT }).then(() => {
-	console.log('HTTP Server running!');
-});
+app.listen({ port: env.PORT });
