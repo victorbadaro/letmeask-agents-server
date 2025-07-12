4f5c3eef359a195978f641d83cab262e7ac3751e
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-07-12 18:18:37 -0300

    feat: add route to get room questions

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
 import { createRoomRoute } from './http/routes/create-room-route.ts';
+import { getRoomQuestions } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -24,5 +25,6 @@ app.get('/health', () => {
 
 app.register(getRoomsRoute);
 app.register(createRoomRoute);
+app.register(getRoomQuestions);
 
 app.listen({ port: env.PORT });
